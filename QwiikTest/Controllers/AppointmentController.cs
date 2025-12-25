096ba6f592ec530871e53de9aade34e1394b9685
funAlways <3865423+funAlways@users.noreply.github.com>
2025-12-25 14:34:12 +0700

    Customer can now choose day of appointment, not just auto select today

--- QwiikTest/Controllers/AppointmentController.cs
+++ QwiikTest/Controllers/AppointmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Agency.DAL.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
 
 namespace QwiikMVC.Controllers
 {
@@ -9,11 +10,34 @@ namespace QwiikMVC.Controllers
         AppointmentService service = new AppointmentService(new AppointmentRepository());
 
         [HttpPost]
-        public ActionResult Book(Appointment appt)
+        public ActionResult Book(Appointment appt, DateTime RequestedDate)
         {
-            var result = service.Book(appt);
-            return RedirectToAction("Confirmation", new { id = result.AppointmentId });
+            var setting = db.AgencySettings.First();
+            DateTime targetDate = RequestedDate;
+
+            while (true)
+            {
+                int count = db.Appointments.Count(a => DbFunctions.TruncateTime(a.AppointmentDate) == targetDate);
+                if (count < setting.MaxAppointmentsPerDay) break;
+                targetDate = targetDate.AddDays(1);
+            }
+
+            int lastToken = db.Appointments
+                .Where(a => DbFunctions.TruncateTime(a.AppointmentDate) == targetDate)
+                .OrderByDescending(a => a.TokenNumber)
+                .Select(a => (int?)a.TokenNumber)
+                .FirstOrDefault() ?? 0;
+
+            appt.TokenNumber = lastToken + 1;
+            appt.Status = "Pending";
+            appt.AppointmentDate = targetDate;
+
+            db.Appointments.Add(appt);
+            db.SaveChanges();
+
+            return RedirectToAction("Confirmation", new { id = appt.AppointmentId });
         }
+
 
         public ActionResult Book()
         {
